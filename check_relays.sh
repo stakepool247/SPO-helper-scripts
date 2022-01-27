09b7bbefea49a1251cf1039756ca5b98ad2dbb71
StakePool247 <59128747+stakepool247@users.noreply.github.com>
2022-01-27 13:30:54 +0200

    Create check_relays.sh
    initial commit

---
+++ check_relays.sh
@@ -0,0 +1,37 @@
+#!/bin/bash    
+clear
+PATH_TO_TOPOLOGY=/home/cardano/cnode/config/top/topology.json
+PATH_TO_TESTED=/home/cardano/cnode/config/top/output.json
+
+wget -q -O ${PATH_TO_TOPOLOGY} https://explorer.mainnet.cardano.org/relays/topology.json
+
+COUNT=$(cat $PATH_TO_TOPOLOGY | jq -r '.Producers | .[] | .addr' | wc -l)
+let NWMAGIC=764824073
+let COUNT-=1
+
+echo "Total Peers in the file: ${COUNT}"
+
+until [  $COUNT -lt 0 ]; do
+        printf "Peer $COUNT: "
+
+        peerIP=$(cat $PATH_TO_TOPOLOGY | jq -r ".Producers | .[$COUNT] | .addr")
+        peerPORT=$(cat $PATH_TO_TOPOLOGY | jq -r ".Producers | .[$COUNT] | .port")
+
+        checkPEER=$(cncli ping --host "${peerIP}" --port "${peerPORT}" --network-magic "${NWMAGIC}")
+        #echo "OUTPUT ${checkPEER}"
+
+        if [[ $(jq -r .status <<< "${checkPEER}") = "ok" ]]; then
+                [[ ${CNCLI_CONNECT_ONLY} = true ]] && peerRTT=$(jq -r .connectDurationMs <<< "${checkPEER}") || peerRTT=$(jq -r .durationMs <<< "${checkPEER}")
+        else # cncli ping failed
+                peerRTT=99999
+        fi
+
+        echo "Peer $COUNT: peerIP: ${peerIP} peerPort: ${peerPORT} peerRTT: ${peerRTT}"
+        let COUNT-=1
+        
+        cat <<EOF >> ${PATH_TO_TESTED}
+        {"address" : "${peerIP}", "port": ${peerPORT}, "rtt": ${peerRTT}}       
+EOF
+done
+
+cat output.json | jq -s -c 'sort_by(.rtt) | .[]' | head -n 20 | sponge ${PATH_TO_TESTED} 
